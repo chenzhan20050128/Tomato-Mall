1af4ed1d27b1cd083396e153cac71310014458e3
Spark <231098201@smail.nju.edu.cn>
2025-03-27 14:40:31 +0800

    前端框架初始化

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,54 +1,43 @@
-import {axios} from '../utils/request'
-import {USER_MODULE} from './_prefix'
+import request from '../utils/request'
 
-type LoginInfo = {
-    phone: string,
-    password: string
-}
-
-type RegisterInfo = {
-    role: number,
-    username: string,
-    phone: string,
-    password: string,
-}
-
-type UpdateInfo = {
-    username?: string,
-    password?: string,
-    address?: string,
-}
-
-// 如果有“Vue: This may be converted to an async function”警告，可以不管
 // 用户登录
-export const userLogin = (loginInfo: LoginInfo) => {
-    return axios.post(`${USER_MODULE}/login`, null, {params: loginInfo})
-        .then(res => {
-            return res
-        })
+export function userLogin(data: {
+  username: string,  // 修改为username，可以是用户名或手机号
+  password: string
+}) {
+  return request({
+    url: '/api/accounts/login',  // 调整为后端API路径
+    method: 'post',
+    data
+  })
 }
 
 // 用户注册
-export const userRegister = (registerInfo: RegisterInfo) => {
-    return axios.post(`${USER_MODULE}/register`, registerInfo,
-        {headers: {'Content-Type': 'application/json'}})
-        .then(res => {
-            return res
-        })
+export function userRegister(data: {
+  username: string,
+  password: string,
+  role: string
+}) {
+  return request({
+    url: '/api/accounts',  // 调整为后端API路径
+    method: 'post',
+    data
+  })
 }
 
 // 获取用户信息
-export const userInfo = () => {
-    return axios.get(`${USER_MODULE}`)
-        .then(res => {
-            return res
-        })
+export function userInfo(username: string) {
+  return request({
+    url: `/api/accounts/${username}`,
+    method: 'get'
+  })
 }
 
 // 更新用户信息
-export const userInfoUpdate = (updateInfo: UpdateInfo) => {
-    return axios.post(`${USER_MODULE}`, updateInfo, {headers: {'Content-Type': 'application/json'}})
-        .then(res => {
-            return res
-        })
+export function updateUserInfo(data: any) {
+  return request({
+    url: '/api/accounts',
+    method: 'put',
+    data
+  })
 }
